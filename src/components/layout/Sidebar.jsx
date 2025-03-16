99405a902f3c1514612c067b1e0901c12b68af3e
abdellatif ibnessayeh <abdellatifibnessayeh@gmail.com>
2025-03-16 17:23:44 +0000

    add Sidebar & SidebarMenuItem component

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import SidebarMenuItem from '../navigation/SidebarMenuItem';
+import { FaShoppingBag, FaShoppingCart, FaChartBar, FaUsers, FaCog, FaLock, FaFileAlt, FaChartPie, FaChartLine } from 'react-icons/fa';
+import { FaShop } from 'react-icons/fa6';
+import { MdManageAccounts } from 'react-icons/md';
 
-const Sidebar = () => {
+const Sidebar = ({activeItem,  onItemClick }) => {
+
+  const menuItems = [
+
+    { id: 'commercial', icon: <FaShop />,  label: 'Gestion commerciale' },
+     { id: 'achats', icon: <FaShoppingCart />, label: 'gestion des achats' },
+    { id: 'dashboard', icon: <FaChartLine />, label: 'Tableau de bord' },
+    { id: 'users', icon: <FaUsers />,  label: 'utilisateurs' },
+     { id: 'params', icon: <FaCog />, label: 'Parametres' },
+
+    { id: 'access', icon: <MdManageAccounts />, label: 'Gestion des accès' },
+    { id: 'reports', icon: <FaFileAlt />,  label: 'Rapports' },
+    { id: 'stats', icon: <FaChartBar />, label: 'Statistiques' }
+  ];
+
   return (
-    <>
-      
-    </>
-  )
-}
+    <div className="w-64 bg-gray-50 h-screen border-r">
+      <div className="p-3">
+        <h5 className="text-sm font-semibold text-gray-500">NAVIGATION</h5>
+      </div>
+      <div>
+        {/* menuItems.slice(0, 5) pour mapper juste sur les 5 preemier item pour la prtie navigation */}
+        {menuItems.slice(0, 5).map(item => (<SidebarMenuItem key= {item.id} icon ={item.icon} label = {item.label}  isActive={activeItem === item.id}  onClick={() => onItemClick(item.id)}/>))}
+      </div>
+      <div className="p-3 mt-3">
+        <h5 className="text-sm font-semibold text-gray-500">ADMINISTRATION</h5>
+      </div>
+      <div>
+                {/* menuItems.slice( 5) pour mapper juste sur les  dernier item pour la prtie administarion */}
+        {menuItems.slice(5).map(item => (<SidebarMenuItem  key ={item.id} icon= {item.icon}  label={item.label} isActive ={activeItem === item.id} onClick={() => onItemClick(item.id)} />))}
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/pages/SalesOrderPage.jsx
+++ src/pages/SalesOrderPage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaEdit } from "react-icons/fa"
 import PageHeader from '../components/layout/PageHeader'
 import ActionBar from '../components/layout/ActionBar'
+import Sidebar from '../components/layout/Sidebar'
 
 const SalesOrderPage = () => {
+    const [activeMenuItem, setActiveMenuItem] = useState('commercial');
   return (
     <>
+    <Sidebar activeItem={activeMenuItem} onItemClick={setActiveMenuItem} />
     <PageHeader icon={<FaEdit />} title="Modifier"></PageHeader>
     <ActionBar></ActionBar>
     </>
